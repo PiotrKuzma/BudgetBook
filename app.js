5220c659e3ca487a6a4f6bcca23e81c3048dd3d2
unknown <kuzma.boh@gmail.com>
2020-01-27 22:41:29 +0100

    UI improvements

--- app.js
+++ app.js
@@ -97,11 +97,11 @@ const UIupdater = (function() {
       } else if (type === "exp") {
         DomNode = DOMselectors.expenseList;
         HTML = `     
-                      <div class="item" id="expense-%dataID%">
-                        <div class="item__description">%%Apartment rent%%</div>
+                      <div class="item" id="expense-${object.id}">
+                        <div class="item__description">${object.description}</div>
                           
-                        <div class="item__value">- %value%</div>
-                        <div class="item__percentage">%%21%%</div>
+                        <div class="item__value">- ${object.value}</div>
+                        <div class="item__percentage">%%</div>
                         <div class="item__delete">
                             <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
                         </div>
